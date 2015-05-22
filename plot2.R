a4cd5ad7d1fe6b3b0c6d3240155ba3d73476662c
wondertrap <rgsmolin@gmail.com>
2015-05-22 14:43:22 +0200

    Second Plot
    plot2.png and plot2.R

---
+++ plot2.R
@@ -0,0 +1,28 @@
+# Coursera - Exploring Data Analysis - Assigment 2 - plot2
+
+# Preparation
+path.dir <- getwd();
+data.dir <- paste0(path.dir,"/exdata-data-NEI_data/")
+
+# Loads RDS
+
+dataPM25 <- readRDS(paste0(data.dir,"summarySCC_PM25.rds"))
+dataCLSS <- readRDS(paste0(data.dir,"Source_Classification_Code.rds"))
+
+# Subsetting data
+# Balitmore City, Maryland
+dataPM25Sub<-subset(dataPM25, fips == "24510")
+
+# Aggregate data
+totalEmissionYear <- aggregate(Emissions ~ year, dataPM25Sub, sum)
+
+# Create and write png bar plot
+png(filename = "plot2.png")
+barplot(
+    (totalEmissionYear$Emissions)/10^3,
+    names.arg=totalEmissionYear$year,
+    xlab="Year",
+    ylab="Total"~ PM[2.5] ~ "Emissions (10^3 Tons)",
+    main="Total Balitmore City " ~ PM[2.5] ~ "Emissions per Year"
+)
+dev.off()
